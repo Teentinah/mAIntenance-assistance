f371c2f4594ccf61ad8a03b64721630b5412dac3
Coder_RM <111121054+Teentinah@users.noreply.github.com>
2026-08-13 16:24:09 +0300

    Delete maintenance-assistance directory

--- maintenance-assistance/run.ps1
+++
@@ -1,5 +0,0 @@
-Write-Host "Installation des dépendances..."
-python -m pip install -r requirements.txt
-
-Write-Host "Lancement de l'interface de démonstration (Streamlit)..."
-python -m streamlit run ui/streamlit_app.py
--- maintenance-assistance/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-echo "Installation des dépendances..."
-python -m pip install -r requirements.txt
-
-echo "Lancement de l'interface de démonstration (Streamlit)..."
-python -m streamlit run ui/streamlit_app.py
